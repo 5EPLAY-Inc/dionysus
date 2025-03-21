3735587b1e8c5b9fca8cc56b8210c84f9af2bce0
赵世鑫 <zhaoshixin@5eplay.com>
2025-03-21 18:26:12 +0800

    feat(registry): 新增nacos服务注册和发现示例

---
+++ example/grpc/registry/client/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/gowins/dionysus/grpc/balancer/resolver"
+	"github.com/gowins/dionysus/grpc/registry"
+	"github.com/gowins/dionysus/grpc/registry/nacos"
+	"github.com/nacos-group/nacos-sdk-go/v2/common/constant"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/keepalive"
+	"log"
+	"sync"
+	"time"
+
+	"github.com/gowins/dionysus/example/grpc/hw"
+	"github.com/gowins/dionysus/grpc/client/pool"
+	xlog "github.com/gowins/dionysus/log"
+	"google.golang.org/grpc/metadata"
+)
+
+var DefaultMaxRecvMsgSize = 1024 * 1024 * 4
+
+// DefaultMaxSendMsgSize maximum message that client can send
+// (4 MB).
+var DefaultMaxSendMsgSize = 1024 * 1024 * 4
+
+var clientParameters = keepalive.ClientParameters{
+	Time:                10 * time.Second, // send pings every 10 seconds if there is no activity
+	Timeout:             2 * time.Second,  // wait 2 second for ping ack before considering the connection dead
+	PermitWithoutStream: true,             // send pings even without active streams
+}
+
+var defaultDialOpts = []grpc.DialOption{
+	grpc.WithTransportCredentials(insecure.NewCredentials()),
+	grpc.WithBlock(),
+	grpc.WithKeepaliveParams(clientParameters),
+	grpc.WithDefaultCallOptions(
+		grpc.MaxCallRecvMsgSize(DefaultMaxRecvMsgSize),
+		grpc.MaxCallSendMsgSize(DefaultMaxSendMsgSize)),
+}
+
+func main() {
+	xlog.Setup(xlog.SetProjectName("grpc-client"))
+	err := registry.Init("nacos://172.16.8.129:8848?secure=false&timeout=30s", nacos.WithClientConfig(&constant.ClientConfig{
+		NamespaceId:         "public", // 如果需要支持多namespace，我们可以创建多个client,它们有不同的NamespaceId。当namespace是public时，此处填空字符串。
+		TimeoutMs:           5000,
+		NotLoadCacheAtStart: true,
+		LogDir:              "/tmp/nacos/log",
+		CacheDir:            "/tmp/nacos/cache",
+		LogLevel:            "debug",
+		AppName:             "dionysus",
+	}))
+
+	if err != nil {
+		panic(err)
+	}
+
+	target := resolver.DiscovScheme + "://nacos" + "/helloworld-rpc"
+	gPool, err := pool.GetGrpcPool(target)
+	if err != nil {
+		fmt.Printf("grpc pool init dial error %v\n", err)
+	}
+	var wg sync.WaitGroup
+	for i := 0; i < 1; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			c := hw.NewGreeterClient(gPool)
+			// Contact the server and print out its response.
+			mdCtx := metadata.NewOutgoingContext(context.Background(), metadata.New(map[string]string{"k": "v"}))
+			r, err := c.SayHello(mdCtx, &hw.HelloRequest{Name: "nameing"})
+			if err != nil {
+				log.Printf("could not greet: %v", err)
+				return
+			}
+			log.Printf("Greeting: %s", r.GetMessage())
+		}()
+	}
+	wg.Wait()
+	//sigs := make(chan os.Signal, 1)
+	//signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	select {}
+}
---
+++ example/grpc/registry/server/main.go
@@ -0,0 +1,172 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/gowins/dionysus/grpc/registry"
+	"github.com/gowins/dionysus/grpc/registry/nacos"
+	"github.com/gowins/dionysus/step"
+	"github.com/nacos-group/nacos-sdk-go/v2/clients"
+	"github.com/nacos-group/nacos-sdk-go/v2/clients/naming_client"
+	"github.com/nacos-group/nacos-sdk-go/v2/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/v2/vo"
+	"github.com/zeromicro/go-zero/core/netx"
+	"log"
+	"net"
+	"strconv"
+	"time"
+
+	"github.com/gowins/dionysus"
+	"github.com/gowins/dionysus/cmd"
+	"github.com/gowins/dionysus/example/grpc/hw"
+	"github.com/gowins/dionysus/grpc/server"
+	"github.com/gowins/dionysus/grpc/serverinterceptors"
+	"google.golang.org/grpc/metadata"
+)
+
+type gserver struct {
+	hw.UnimplementedGreeterServer
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *gserver) SayHello(ctx context.Context, in *hw.HelloRequest) (*hw.HelloReply, error) {
+	md, _ := metadata.FromIncomingContext(ctx)
+	fmt.Printf("MetaData: %#v \n", md)
+	log.Printf("Received: %v", in.GetName())
+	return &hw.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
+
+var nacosNamingClient naming_client.INamingClient
+
+func main() {
+	dio := dionysus.NewDio()
+	cfg := server.DefaultCfg
+	cfg.Address = ":8081"
+	serviceID := uuid.New().String()
+	c := cmd.NewGrpcCmd(cmd.WithCfg(cfg))
+	c.EnableDebug()
+	// timeout interceptor
+	c.AddUnaryServerInterceptors(serverinterceptors.TimeoutUnary(10 * time.Second))
+	// recover interceptor
+	c.AddUnaryServerInterceptors(serverinterceptors.RecoveryUnary(serverinterceptors.DefaultRecovery()))
+	c.AddStreamServerInterceptors(serverinterceptors.RecoveryStream(serverinterceptors.DefaultRecovery()))
+	// tacing interceptor
+	c.AddUnaryServerInterceptors(serverinterceptors.OpenTracingUnary())
+	c.AddStreamServerInterceptors(serverinterceptors.OpenTracingStream())
+
+	// register grpc service
+	c.RegisterGrpcService(hw.RegisterGreeterServer, &gserver{})
+	preSteps := []step.InstanceStep{
+		{
+			StepName: "register service to regtriy",
+			Func: func() error {
+
+				err := registry.Init("nacos://172.16.8.129:8848?secure=false&timeout=30s", nacos.WithClientConfig(&constant.ClientConfig{
+					NamespaceId:         "public", // 如果需要支持多namespace，我们可以创建多个client,它们有不同的NamespaceId。当namespace是public时，此处填空字符串。
+					TimeoutMs:           5000,
+					NotLoadCacheAtStart: true,
+					LogDir:              "/tmp/nacos/log",
+					CacheDir:            "/tmp/nacos/cache",
+					LogLevel:            "debug",
+					AppName:             "dionysus",
+				}))
+
+				if err != nil {
+					panic(err)
+				}
+
+				registryClient := registry.Get(nacos.Name)
+				_, port, _ := net.SplitHostPort(cfg.Address)
+				p, _ := strconv.Atoi(port)
+				err = registryClient.Register(&registry.Service{
+					Name:    "helloworld-rpc",
+					Version: "1.0.0",
+					Metadata: map[string]string{
+						"kind":    "grpc",
+						"version": "",
+					},
+					Nodes: []*registry.Node{
+						{
+							Id: serviceID,
+							Metadata: map[string]string{
+								"kind":    "grpc",
+								"version": "",
+							},
+							Address: netx.InternalIp(),
+							Port:    p,
+						},
+					},
+				})
+				if err != nil {
+					panic(err)
+				}
+				return nil
+			},
+		},
+	}
+
+	postSteps := []step.InstanceStep{
+		{
+			StepName: "deregister service from registry",
+			Func: func() error {
+				err := registry.Init("nacos://172.16.8.129:8848?secure=false&timeout=30s", nacos.WithClientConfig(&constant.ClientConfig{
+					NamespaceId:         "public", // 如果需要支持多namespace，我们可以创建多个client,它们有不同的NamespaceId。当namespace是public时，此处填空字符串。
+					TimeoutMs:           5000,
+					NotLoadCacheAtStart: true,
+					LogDir:              "/tmp/nacos/log",
+					CacheDir:            "/tmp/nacos/cache",
+					LogLevel:            "debug",
+					AppName:             "dionysus",
+				}))
+
+				if err != nil {
+					panic(err)
+				}
+
+				registryClient := registry.Get(nacos.Name)
+				_, port, _ := net.SplitHostPort(cfg.Address)
+				p, _ := strconv.Atoi(port)
+				err = registryClient.Deregister(&registry.Service{
+					Name: "helloworld-rpc",
+					Nodes: []*registry.Node{
+						{
+							Id:      serviceID,
+							Address: netx.InternalIp(),
+							Port:    p,
+						},
+					},
+				})
+				if err != nil {
+					return err
+				}
+				return nil
+			},
+		},
+	}
+	dio.PreRunStepsAppend(preSteps...)
+	dio.PostRunStepsAppend(postSteps...)
+	_ = postSteps
+	dio.DioStart("grpc", c)
+}
+
+func SetupNacosNamingClient() (naming_client.INamingClient, error) {
+	clientConfig := constant.ClientConfig{
+		NamespaceId:         "e525eafa-f7d7-4029-83d9-008937f9d468", // 如果需要支持多namespace，我们可以创建多个client,它们有不同的NamespaceId。当namespace是public时，此处填空字符串。
+		TimeoutMs:           5000,
+		NotLoadCacheAtStart: true,
+		LogDir:              "/tmp/nacos/log",
+		CacheDir:            "/tmp/nacos/cache",
+		LogLevel:            "debug",
+		AppName:             "dionysus",
+	}
+	serverConfigs := []constant.ServerConfig{
+		*constant.NewServerConfig("mse-af860a50-nacos-ans.mse.aliyuncs.com", 8848),
+	}
+	return clients.NewNamingClient(
+		vo.NacosClientParam{
+			ClientConfig:  &clientConfig,
+			ServerConfigs: serverConfigs,
+		},
+	)
+}
